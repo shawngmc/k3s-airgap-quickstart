288bf2d1993c8a58b0c493f6cf60032d1adbb981
shawngmc <shawngmc@gmail.com>
2023-12-05 09:47:00 -0500

    whitespace

--- download/110-download-helm.sh
+++ download/110-download-helm.sh
@@ -6,4 +6,3 @@ source .env
 HELM_EXEC_URL="https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz"
 wget "${HELM_EXEC_URL}" -O ${GAP_DIR}/helm-${HELM_VERSION}-linux-amd64.tar.gz
 echo "${HELM_EXEC_URL}" >> ${GAP_DIR}/urls.txt
-
