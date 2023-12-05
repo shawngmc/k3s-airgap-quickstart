8beaa270e4e7aecd5ae42d88ba7f0796d79acbd0
shawngmc <7342147+shawngmc@users.noreply.github.com>
2023-12-04 23:01:17 -0500

    Update 010-install-crane.sh

--- install/010-install-crane.sh
+++ install/010-install-crane.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-TEMP_DIR=~/tmp/k3s
-
-CRANE_VERSION=$(curl -s "https://api.github.com/repos/google/go-containerregistry/releases/latest" | jq -r '.tag_name')
+source .env
 
 # Download Crane
 curl -sL "https://github.com/google/go-containerregistry/releases/download/${VERSION}/go-containerregistry_${OS}_${ARCH}.tar.gz" > ${TEMP_DIR}/go-containerregistry.tar.gz
