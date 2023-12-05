f5662ea54c8f44468cc3a70830904d2c9ec45d84
Shawn McNaughton <shawngmc@gmail.com>
2023-12-05 04:53:36 +0000

    more fixes

--- install/100-install-calico.sh
+++ install/100-install-calico.sh
@@ -5,16 +5,15 @@ source .env
 # Download Calico AirGap
 wget https://raw.githubusercontent.com/projectcalico/calico/${CALICO_VERSION}/manifests/calico.yaml -O ${TEMP_DIR}/calico.yaml
 CALICO_IMAGES=( cni node kube-controllers )
-for $CALICO_IAMGE in "${CALICO_IMAGES[@]}"
+for CALICO_IMAGE in "${CALICO_IMAGES[@]}"
 do
-  echo "Value for fruits array is: $value"
-  crane pull docker.io/calico/${CALICO_IMAGE}:${CALICO_VERSION} ${TEMP_DIR}/calico-${CALICO_IMAGE}-${CALICO_VERSION}.tar.gz
-  gzip -d ${TEMP_DIR}/calico-${CALICO_IMAGE}-${CALICO_VERSION}.tar.gz  
-  kubectl ctr images import ${TEMP_DIR}/calico-${CALICO_IMAGE}-${CALICO_VERSION}.tar
+  crane pull docker.io/calico/${CALICO_IMAGE}:${CALICO_VERSION} ${TEMP_DIR}/calico-${CALICO_IMAGE}-${CALICO_VERSION}.tar
+  sudo k3s ctr images import ${TEMP_DIR}/calico-${CALICO_IMAGE}-${CALICO_VERSION}.tar
 done
+
 wget https://github.com/projectcalico/calico/releases/download/v3.26.4/calicoctl-linux-amd64 -O ${TEMP_DIR}/calicoctl
-sudo cp ${TEMP_DIR}/calicoctl /usr/local/bin/calicoctl
-sudo chmod 755 /usr/local/bin/calicoctl
+sudo cp ${TEMP_DIR}/calicoctl /usr/local/bin/calicoctl
+sudo chmod 755 /usr/local/bin/calicoctl
 
 # Install Calico
-kubectl apply -f calico.yaml
+kubectl apply -f ${TEMP_DIR}/calico.yaml
