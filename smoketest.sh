d71b1ff11b3bd96c110b6f129b799758abb46b4c
Shawn McNaughton <shawngmc@gmail.com>
2023-12-05 05:18:10 +0000

    working smoketest

--- smoketest.sh
+++ smoketest.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-# TODO: Pull nginx, run it, verify
+echo "Creating NGINX deployment..."
+kubectl create deployment nginx --image=nginx
+sleep 10
 
+echo "Exposing NodePort..."
+kubectl expose deployment nginx --port 80 --type NodePort
+sleep 5
+
+NODE_PORT=$(kubectl get svc nginx \
+	  --output=jsonpath='{range .spec.ports[0]}{.nodePort}')
+echo "Testing nginx deployment at http://127.0.0.1/${NODE_PORT}..."
+curl -I http://127.0.0.1:${NODE_PORT}
+
+echo "Cleaning up NGINX deployment and service..."
+kubectl delete svc nginx
+kubectl delete deployment nginx
