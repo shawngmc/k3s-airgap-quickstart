2168c8eb3c73cd3d55484d0a1d192e75252c16db
Shawn McNaughton <shawngmc@gmail.com>
2023-12-05 04:37:38 +0000

    fixing encoding issues

--- install/010-install-crane.sh
+++ install/010-install-crane.sh
@@ -3,7 +3,7 @@
 source .env
 
 # Download Crane
-curl -sL "https://github.com/google/go-containerregistry/releases/download/${VERSION}/go-containerregistry_${OS}_${ARCH}.tar.gz" > ${TEMP_DIR}/go-containerregistry.tar.gz
+wget "https://github.com/google/go-containerregistry/releases/download/${CRANE_VERSION}/go-containerregistry_${OS}_${ARCH}.tar.gz" -O ${TEMP_DIR}/go-containerregistry.tar.gz
 
 # Install Crane
 sudo tar -zxvf ${TEMP_DIR}/go-containerregistry.tar.gz -C /usr/local/bin/ crane
--- install/050-install-k3s.sh
+++ install/050-install-k3s.sh
@@ -4,11 +4,11 @@ source .env
 
 # Download K3s
 wget https://github.com/k3s-io/k3s/releases/download/${K3S_VERSION}/k3s -O $TEMP_DIR/k3s
-wget https://github.com/k3s-io/k3s/releases/download/${K3S_VERSION}/k3s-airgap-images-amd64.tar -O $TEMP_DIR/k3s-airgap-images-amd64.tar
-wget https://get.k3s.io/ -O $TEMP_DIR/install.sh
+wget https://github.com/k3s-io/k3s/releases/download/${K3S_VERSION}/k3s-airgap-images-amd64.tar -O "${TEMP_DIR}/k3s-airgap-images-amd64.tar"
+wget https://get.k3s.io/ -O ${TEMP_DIR}/install.sh
 # Stage images
 sudo mkdir -p /var/lib/rancher/k3s/agent/images/
-sudo cp ${TEMP_DIR}/k3s-airgap-images-amd64.tar /var/lib/rancher/k3s/agent/images/
+sudo cp ${TEMP_DIR}/k3s-airgap-images-amd64.tar /var/lib/rancher/k3s/agent/images/
 sudo chmod 755 /var/lib/rancher
 sudo chmod 755 /var/lib/rancher/k3s
 sudo chmod 755 /var/lib/rancher/k3s/agent
@@ -16,17 +16,19 @@ sudo chmod 755 /var/lib/rancher/k3s/agent/images
 sudo chmod 644 /var/lib/rancher/k3s/agent/images/k3s-airgap-images-amd64.tar
 
 # Stage executable
-sudo cp ${TEMP_DIR}/k3s /usr/local/bin/k3s
-sudo chmod 755 /usr/local/bin/k3s
+sudo cp ${TEMP_DIR}/k3s /usr/local/bin/k3s
+sudo chmod 755 /usr/local/bin/k3s
 k3s --version
+
 # Create config
-sudo mkdir -pv /etc/rancher/k3s
+sudo mkdir -pv /etc/rancher/k3s
 sudo tee "/etc/rancher/k3s/config.yaml" > /dev/null <<'EOF'
 write-kubeconfig-mode: "0644"
 disable-network-policy: true
 flannel-backend: none
 EOF
-sudo chmod 644 /etc/rancher/k3s/config.yaml
+sudo chmod 644 /etc/rancher/k3s/config.yaml
+
 # Install k3s airgap
 chmod 744 ${TEMP_DIR}/install.sh
 INSTALL_K3S_SKIP_DOWNLOAD=true ${TEMP_DIR}/install.sh
