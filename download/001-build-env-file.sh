21ee2f6130eff53e65833d5c49d54329bba2049e
Shawn McNaughton <shawngmc@gmail.com>
2023-12-05 14:35:02 +0000

    .env fix

--- download/001-build-env-file.sh
+++ download/001-build-env-file.sh
@@ -4,12 +4,12 @@ source .env
 
 mkdir -pv ${GAP_DIR}/temp
 
-sudo tee "${GAP_DIR}/.env" > /dev/null <<'EOF'
+sudo tee "${GAP_DIR}/.env" > /dev/null <<EOF
 GAP_DIR=../gapfiles
 OS=Linux
 ARCH=x86_64
-CRANE_VERSION=$(CRANE_VERSION)
+CRANE_VERSION=${CRANE_VERSION}
 K3S_VERSION=v1.28.3\%2Bk3s2
 CALICO_VERSION=v3.26.3
-HELM_VERSION=$(HELM_VERSION)
+HELM_VERSION=${HELM_VERSION}
 EOF
