da1cae512247601c9d8455f2b5d0e62f2b4d843f
shawngmc <7342147+shawngmc@users.noreply.github.com>
2023-12-13 18:00:10 -0500

    Update 015-uninstall-calico.sh

--- uninstall/015-uninstall-calico.sh
+++ uninstall/015-uninstall-calico.sh
@@ -5,5 +5,6 @@ source .env
 # Uninstall Calico
 kubectl delete -f ${GAP_DIR}/calico.yaml
 
-# Clean up CNI
-sudo rm -rf /etc/cni/net.d/*calico*
+# Clean up CNI - see https://stackoverflow.com/questions/61672804/after-uninstalling-calico-new-pods-are-stuck-in-container-creating-state
+sudo rm -rf /etc/cni/net.d/calico-kubeconfig
+sudo rm -rf /etc/cni/net.d/10-calico.conflist
