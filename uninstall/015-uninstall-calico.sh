1e22a51a59f47fe57f5c523d65d2ec90e2631a6b
shawngmc <7342147+shawngmc@users.noreply.github.com>
2023-12-13 19:26:18 -0500

    Update 015-uninstall-calico.sh remove images

--- uninstall/015-uninstall-calico.sh
+++ uninstall/015-uninstall-calico.sh
@@ -8,3 +8,10 @@ kubectl delete -f ${GAP_DIR}/calico.yaml
 # Clean up CNI - see https://stackoverflow.com/questions/61672804/after-uninstalling-calico-new-pods-are-stuck-in-container-creating-state
 sudo rm -rf /etc/cni/net.d/calico-kubeconfig
 sudo rm -rf /etc/cni/net.d/10-calico.conflist
+
+# Remove images
+CALICO_IMAGES=( cni node kube-controllers )
+for CALICO_IMAGE in "${CALICO_IMAGES[@]}"
+do
+  sudo k3s ctr images del docker.io/calico/${CALICO_IMAGE}:${CALICO_VERSION}
+done
