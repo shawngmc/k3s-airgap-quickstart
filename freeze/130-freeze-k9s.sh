4e9b3ca436e7e5039d4eb864f8915239e5a64e4a
Shawn <shawngmc@gmail.com>
2024-01-12 00:20:29 -0500

    fix quote typo for k9s freeze

--- freeze/130-freeze-k9s.sh
+++ freeze/130-freeze-k9s.sh
@@ -5,5 +5,5 @@ source "${GAP_DIR}/.env"
 K9S_VERSION=$(curl -s "https://api.github.com/repos/derailed/k9s/releases/latest" | jq -r '.tag_name')
 
 cat << EOF >> "${GAP_DIR}/.env"
-K9S_VERSION=${K9S_VERSION}"
+K9S_VERSION=${K9S_VERSION}
 EOF
