5adbe9703e7e6e758adec3a7bb5b26c82869d8e3
shawngmc <7342147+shawngmc@users.noreply.github.com>
2024-03-21 13:52:12 -0400

    Update and rename 120-freeze-longhorn.sh to 120-freeze-csi-local-static.sh

---
+++ freeze/120-freeze-csi-local-static.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source "${GAP_DIR}/.env" 
+
+helm repo add sig-storage-local-static-provisioner https://kubernetes-sigs.github.io/sig-storage-local-static-provisioner/
+CSI_LOCAL_STATIC_VERSION=$(helm show chart sig-storage-local-static-provisioner/local-static-provisioner | yq -r .version)
+
+cat << EOF >> "${GAP_DIR}/.env"
+CSI_LOCAL_STATIC_VERSION=${CSI_LOCAL_STATIC_VERSION}
+EOF
