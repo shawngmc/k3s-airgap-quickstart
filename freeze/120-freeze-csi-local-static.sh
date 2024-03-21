5b5f93d2fcd69e9ace44a5583020eff2e5e64b63
shawngmc <7342147+shawngmc@users.noreply.github.com>
2024-03-21 14:00:52 -0400

    Update 120-freeze-csi-local-static.sh

--- freeze/120-freeze-csi-local-static.sh
+++ freeze/120-freeze-csi-local-static.sh
@@ -5,9 +5,11 @@ source "${GAP_DIR}/.env"
 CSI_LOCAL_STATIC_CHART_VERSION=$(helm show chart https://kubernetes-sigs.github.io/sig-storage-local-static-provisioner/local-static-provisioner | yq -r .version)
 
 # TODO: Pull from image version from chart:values.yaml:images
-CSI_LOCAL_STATIC_IMAGE="registry.k8s.io/sig-storage/local-volume-provisioner:v2.6.0"
+CSI_LOCAL_STATIC_IMAGE_VERSION="v2.6.0"
+CSI_LOCAL_STATIC_IMAGE="registry.k8s.io/sig-storage/local-volume-provisioner:${CSI_LOCAL_STATIC_IMAGE_VERSION}"
 
 cat << EOF >> "${GAP_DIR}/.env"
 CSI_LOCAL_STATIC_CHART_VERSION=${CSI_LOCAL_STATIC_CHART_VERSION}
 CSI_LOCAL_STATIC_IMAGE=${CSI_LOCAL_STATIC_IMAGE}
+CSI_LOCAL_STATIC_IMAGE_VERSION=${CSI_LOCAL_STATIC_IMAGE_VERSION}
 EOF
